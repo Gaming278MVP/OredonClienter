34d2a21e788207f97bdc40994ec8427fe515f11a
AnakBodohKaliaan <39037579+FaiqGaming@users.noreply.github.com>
2019-01-17 12:39:36 +0900

    Update command.js

--- handle/command.js
+++ handle/command.js
@@ -1,13 +1,16 @@
 const { bot_prefix, embed_color } = require('../config.json');
-const { Collection } = require('discord.js');
-const { RichEmbed } = require('discord.js');
+const { Collection, RichEmbed } = require('discord.js');
 const cooldowns = new Collection();
+const cooldownAns = require('./cooldownAns.json');
+const fs = require('fs');
+const db = require('quick.db');
 
 module.exports = async (client, message) => {
-    let prefix = bot_prefix;
+    let prefix = message.content.startsWith(bot_prefix) ? bot_prefix : `${client.user.toString()} `;
     let color = embed_color;
     let args = message.content.slice(prefix.length).trim().split(/ +/g);
     let cmd = args.shift().toLowerCase();
+    args.missing = argsMissing;
 
     // cooldowns command
     let commandFile = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
@@ -19,6 +22,7 @@ module.exports = async (client, message) => {
     const now = Date.now();
     const timestamps = cooldowns.get(commandFile.help.name);
     const cooldownAmount = (commandFile.conf.cooldown || 5) * 1000;
+    const opss = cooldownAns[Math.floor(Math.random() * cooldownAns.length)];
 
     if (!timestamps.has(member.id)) {
         timestamps.set(member.id, now);
@@ -27,21 +31,33 @@ module.exports = async (client, message) => {
 
         if (now < expirationTime) {
             const timeLeft = (expirationTime - now) / 1000;
-            return message.channel.send(`**${member.user.username}**, please wait **${timeLeft.toFixed(1)}** cooldown time.`).then(msg=>msg.delete(3000));
+            return message.channel.send(`⏱ | **${member.user.username}**, ${opss} (Ratelimited)\n**You'll be able to use this command again in** **${timeLeft.toFixed(1)} seconds.**`).then(msg=>msg.delete(10000));
         }
 
         timestamps.set(member.id, now);
         setTimeout(() => timestamps.delete(member.id), cooldownAmount);
     }
-
+  
     // command handler
   try {
   let commands = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
-  commands.run(client, message, args);
+  commands.run(client, message, args, color, prefix);
   if (!commands) return;
   } catch (e) {
       console.error(e)
   } finally {
-  console.log(`${message.author.tag}[${message.author.id}] is using ${cmd} command on ${message.guild.name}[${message.guild.id}]`);
+  console.info(`${message.author.tag}[${message.author.id}] is using ${message.content.split(" ")[0].replace(prefix, '')} command on shard ﹙${client.shard.id}﹚ ${message.guild.name}[${message.guild.id}]`);
+  db.add('commandUsage', 1);
   }
+} 
+
+function argsMissing(message, res, help){
+  const embed = new RichEmbed()
+	.setColor('#FF1000')
+	.setTitle('⛔ | It\'s not how you use '+ help.name)
+	.addField('❓ Reason', `\`\`\`${res}\`\`\``)
+	.addField('<:usage:501551862017818636> Usage', `\`\`\`${help.usage}\`\`\``)
+//	.addField('Example', help.example.map(x => `\`\`\`${x}\`\`\``));
+	return message.channel.send(embed);
+}
 }
