0370c1a9ec3c6c2c94fb61b98142592343282848
AnakBodohKaliaan <39037579+FaiqGaming@users.noreply.github.com>
2019-01-17 12:00:44 +0900

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const fetch = require("superagent");
-      fetch.get(`https://spiffy-lock.glitch.me/`).set('user-agent', 'official-pinger/1.0.0').then(x => x);
-    setInterval(() => {
-      fetch.get(`https://spiffy-lock.glitch.me/`).set('user-agent', 'official-pinger/1.0.0').then(x => x);
-    }, 180000); 
-    setInterval(() => {
-      fetch.get(`https://spiffy-lock.glitch.me/`).set('user-agent', 'official-pinger/1.0.0').then(x => x);
-    }, 240000);
