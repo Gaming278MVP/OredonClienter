f6cdfe7d4e4601bab608d52ad7f70b03bb263865
AnakBodohKaliaan <39037579+FaiqGaming@users.noreply.github.com>
2019-01-17 11:47:30 +0900

    Update invite.js

--- commands/Support/invite.js
+++ commands/Support/invite.js
@@ -4,8 +4,17 @@ exports.run = (client, msg, args) => {
   let embed = new Discord.RichEmbed()
   .setColor('RANDOM')
   .setTitle(`${client.user.username}'s Invite Link`)
-  .setDescription('==================== \n**[Invite Me!](https://discord.now.sh/511890934460317697)** \n====================')
+  .setDescription('==================== \n**[Invite Me!](https://discordapp.com/oauth2/authorize?client_id=519330418642911237&permissions=2146958839&scope=bot)** \n====================')
   msg.channel.send(embed);
+}
+
+exports.conf = {
+    aliases: [],
+    cooldowns: '5'
+}
+
+exports.help = {
+  name: "invite"
 }
 
 //the code is stop in here
