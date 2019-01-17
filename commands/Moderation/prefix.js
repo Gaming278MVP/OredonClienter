498bc62cd844d059ebc4ee8401c02b3c46d45f62
AnakBodohKaliaan <39037579+FaiqGaming@users.noreply.github.com>
2019-01-17 14:55:38 +0900

    Update prefix.js

--- commands/Moderation/prefix.js
+++ commands/Moderation/prefix.js
@@ -5,12 +5,12 @@ exports.run = async(client, message, args) => {
   if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("You don't have permissions to set prefix!");
   if(!args[0]) return message.channel.send("Please specify a prefix!");
   
-  let oredon = JSON.parse(fs.readFileSync("../../oredon.json", "utf8"));
+  let oredon = JSON.parse(fs.readFileSync("./oredon.json", "utf8"));
   oredon[message.guild.id] = {
     prefix: args[0]
   }
   
-  fs.writeFile("../../oredon.json", JSON.stringify(oredon), (err) => {
+  fs.writeFile("./oredon.json", JSON.stringify(oredon), (err) => {
      if(err) console.log(err);
   })
   
