ed1e11e5868de6beb3e82807c3962370d50e2397
AnakBodohKaliaan <39037579+FaiqGaming@users.noreply.github.com>
2019-01-17 14:58:55 +0900

    Update and rename commands/Fun/cat.js to commands/cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const superagent = require("superagent");
 
-module.exports.run = async(bot, message, args) => {
+exports.run = async(bot, message, args) => {
  
   let {body} = await superagent
   .get('https://aws.random.cat/meow');
