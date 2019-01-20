238e2af689521ed6a09d64f24367fe6253779594
AnakBodohKaliaan <39037579+FaiqGaming@users.noreply.github.com>
2019-01-20 12:14:10 +0900

    Update autorole.js

--- commands/autorole.js
+++ commands/autorole.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
   if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send({ embed: { color: 0xFF0000, description: 'You do not have permissions!'}});
   if (!roles) return message.channel.send("Please provide a mention roles!");
 
-  db.get(`autorole_${message.guild.id}`, roles.id).then(autorole => {
+  db.set(`autorole_${message.guild.id}`, roles.id).then(autorole => {
     message.channel.send(`Succesfully Set AutoRole To Role <@&${autorole}>`)
   })
 }
