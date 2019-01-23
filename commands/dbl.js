06e4252224d48176ba75b7f2286f94312aef4054
AnakBodohKaliaan <39037579+FaiqGaming@users.noreply.github.com>
2019-01-23 20:11:51 +0900

    Update dbl.js

--- commands/dbl.js
+++ commands/dbl.js
@@ -3,7 +3,7 @@ const DBL = require("dblapi.js");
 const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUxOTMzMDQxODY0MjkxMTIzNyIsImJvdCI6dHJ1ZSwiaWF0IjoxNTQ1MTg4Mzk4fQ.uYriOBoJNFgitrVf9edd6_P-xME1bBSlSD19PHBBWsQ');
 
 exports.run = async(client, msg, args) => {
-  let id = message.mentions.members.first()
+  let id = msg.mentions.members.first().user.id;
   let dblBots = dbl.getBots(id)
   
   let embed = new Discord.RichEmbed()
